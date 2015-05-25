419f7dd153c726a4460298ec58bb4fbded3f419a
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2015-05-25 16:30:46 -0400

    Rest Soap and Docker built

---
+++ consuming-rest/src/main/java/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.web.client.RestTemplate;
+
+public class Application {
+
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Page page = restTemplate.getForObject("http://graph.facebook.com/pivotalsoftware", Page.class);
+        System.out.println("Name:    " + page.getName());
+        System.out.println("About:   " + page.getAbout());
+        System.out.println("Phone:   " + page.getPhone());
+        System.out.println("Website: " + page.getWebsite());
+    }
+
+}
